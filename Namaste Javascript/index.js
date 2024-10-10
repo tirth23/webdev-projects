9bd49a919bdc362a55c888ff42629e34b2223020
Tirth Patel <tirthpatel23t@gmail.com>
2024-10-11 01:49:28 +0530

    update: index

--- Namaste Javascript/index.js
+++ Namaste Javascript/index.js
@@ -10,14 +10,15 @@ Object DataType: Object, Array, Function, Map, Set, Date, RegExp, Error, WeakMap
 // var b = undefined;
 // var c = Symbol("Hello");
 // var d = BigInt(10);
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+// console.log(a, typeof a);
+// console.log(b, typeof b);
+// console.log(c, typeof c);
+// console.log(d, typeof d);
 
-// console.log(x);
-// console.log(y);
-// console.log(z);
-// console.log(a);
-// console.log(b);
-// console.log(c);
-// console.log(d);
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
 
 // var x = 7;
 // function getName() {
@@ -26,13 +27,20 @@ Object DataType: Object, Array, Function, Map, Set, Date, RegExp, Error, WeakMap
 // getName();
 // console.log(x);
 
-// /* hoisting */
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
+
+/* 
+hoisting: JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, 
+variables, classes, or imports to the top of their scope, prior to execution of the code
+ */
 // getName();
 // console.log(x);
 // var x = 7;
 // function getName() {
 // 	console.log("Namaste Javascript");
 // }
+
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
 
 // getName();
 // console.log(x);
@@ -42,31 +50,51 @@ Object DataType: Object, Array, Function, Map, Set, Date, RegExp, Error, WeakMap
 // 	console.log("Namaste Javascript");
 // }
 
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
+
 // function getName() {
 // 	console.log("Namaste Javascript");
 // 	// return 2;
 // }
 // console.log(getName());
 
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
+
 // console.log(getName);
 // function getName() {
 // 	console.log("Namaste Javascript");
 // }
 // console.log(getName);
+// console.log(typeof getName);
+
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
 
 // x = 7;
 // console.log(x);
 // var x;
 
-// x = 7;
-// console.log(x);
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
 
 // var x;
 // console.log(x);
 // x = 7;
 
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
+
+// /*
+// In non-strict, if explicitly not declare as var, let or const, it will be declared as global variable
+// In strict mode, if explicitly not declare as var, let or const, it will throw an reference error
+// */
+// // 'use strict'
+// x = 7;
+// console.log(x);
+
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
+
 // console.log(x);
 // x = 7;
+
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
 
 // /* Arrow Function - not hoisted*/
 // var y = 7;
@@ -78,6 +106,8 @@ Object DataType: Object, Array, Function, Map, Set, Date, RegExp, Error, WeakMap
 // 	console.log("Hi");
 // };
 
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
+
 // var y = 7;
 // console.log(y);
 // console.log(getName); //getname is variable in memory which is undefined
@@ -86,6 +116,8 @@ Object DataType: Object, Array, Function, Map, Set, Date, RegExp, Error, WeakMap
 // var getName = () => {
 // 	console.log("Hi");
 // };
+
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
 
 // var y = 7;
 // console.log(y);
@@ -96,6 +128,8 @@ Object DataType: Object, Array, Function, Map, Set, Date, RegExp, Error, WeakMap
 // };
 // getName(); //once function initialized it behaves like function
 
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
+
 // var y = 7;
 // console.log(y);
 // console.log(x);
@@ -104,6 +138,8 @@ Object DataType: Object, Array, Function, Map, Set, Date, RegExp, Error, WeakMap
 // 	console.log("Hi");
 // };
 // console.log(getName); //once function initialized it behaves like function
+
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
 
 // /* Anonyms Function behaves same as arrow function for hoisting */
 // var y = 7;
@@ -117,6 +153,8 @@ Object DataType: Object, Array, Function, Map, Set, Date, RegExp, Error, WeakMap
 // };
 // console.log(getName);
 // getName();
+
+/* ------------------------------------------------------------------------------------------------------------------------------------ */
 
 // /* Named Function Expression behaves same as arrow function for hoisting */
 // var y = 7;
